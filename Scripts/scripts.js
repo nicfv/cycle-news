2a3f93d62fff84bddd9e16421bfc116b2a49cca3
nicfv <omg.my.junkmail@gmail.com>
2019-02-01 14:20:16 -0800

    Removed script

--- Scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-function openclose(id) {
-    var subnav = document.getElementById(id);
-    var stat = subnav.getAttribute("status");
-    if(stat == "open") {
-        subnav.setAttribute("status", "closed");
-    } else {
-        subnav.setAttribute("status", "open");
-    }
-}
